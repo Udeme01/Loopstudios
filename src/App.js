6c852768c51d807044181649e6215c62d3bb339c
Udeme01 <emmanueluddy2@gmail.com>
2024-01-07 20:11:44 +0100

    added Creations component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import Header from './components/Header'
 import Showcase from './components/Showcase';
+import Interactive from './components/Interactive';
+import Creations from './components/Creations';
 
 const App = () => {
   return (
     <>
       <Header />
       <Showcase />
+      <Interactive />
+      <Creations />
     </>
   );
 }
---
+++ src/components/Creations.js
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+import data from "../data-mobile";
+
+const Creations = () => {
+  const [cards, setCards] = useState(data);
+  return (
+    <section className="creation-mobile">
+      <h2>Our Creations</h2>
+      <section>
+        {cards.map((card) => {
+          const { id, title, image } = card;
+          return (
+            <article key={id}>
+              <div className="img-div">
+                <img src={image} alt="creations card" />
+              </div>
+              <h3>{title}</h3>
+            </article>
+          );
+        })}
+      </section>
+    </section>
+  );
+};
+
+export default Creations;
